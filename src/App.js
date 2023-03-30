dc5f69cbe6b4c8b680ef2c530dbed9ede6dae57a
unknown <727721euec055@skcet.ac.in>
2023-03-30 15:24:39 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const Click=()=>
+  {
+    alert("LOGGED IN")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WELCOME TO THIS PAGE</h1>
+<button onClick={window['alertMsg']}>ALLOW ME</button>
+<h1>Message to Developer</h1>
+<button onClick={window['Console']}>CLICK HERE</button>
     </div>
+    
   );
 }
 
